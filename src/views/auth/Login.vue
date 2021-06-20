a8aaeb87f7da97120b7bbac3772d784eee643a38
jose carlos <jose-carlos.garcia-carranza@epitech.eu>
2021-06-20 22:28:18 +0200

    Add register and login pages, send request

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+        <v-container
+            class="fill-height"
+            fluid
+        >
+            <v-row
+            align="center"
+            justify="center"
+            >
+            <v-col
+                cols="12"
+                sm="8"
+                md="4"
+            >
+                <v-card class="elevation-12">
+                <v-toolbar
+                    color="primary"
+                    dark
+                    flat
+                >
+                    <v-toolbar-title>Login form</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                </v-toolbar>
+                <v-card-text>
+                    <v-form ref="loginForm">
+                    <v-text-field
+                        label="Email"
+                        name="email"
+                        prepend-icon="mdi-account"
+                        type="email"
+                        v-model="user.email"
+                    ></v-text-field>
+
+                    <v-text-field
+                        id="password"
+                        label="Password"
+                        name="password"
+                        prepend-icon="mdi-lock"
+                        type="password"
+                        v-model="user.password"
+                    ></v-text-field>
+                    </v-form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" @click="loginUser">Login</v-btn>
+                </v-card-actions>
+                </v-card>
+            </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    name: "Login",
+    data(){
+        return{
+            user:{
+                email: '',
+                password:''
+            }
+        }
+    },
+    methods:{
+        loginUser(){
+            if(this.$refs.loginForm.validate()){
+                axios
+                    .post('http://127.0.0.1:8001/api/login', this.user)
+                    .then((response) => {
+                    
+                        console.log(response.data)
+                        
+                    })
+            }
+        }
+    }
+}
+</script>
---
+++ src/views/auth/Register.vue
@@ -0,0 +1,134 @@
+<template>
+    <div>
+        <v-container
+            class="fill-height"
+            fluid
+        >
+            <v-row
+            align="center"
+            justify="center"
+            >
+            <v-col
+                cols="12"
+                sm="8"
+                md="4"
+            >
+                <v-card class="elevation-12">
+                <v-toolbar
+                    color="primary"
+                    dark
+                    flat
+                >
+                    <v-toolbar-title>Register form</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                </v-toolbar>
+                <v-card-text>
+                    <v-form ref="registerForm" :value="formValid">
+                    <v-text-field
+                        label="Name"
+                        name="name"
+                        prepend-icon="mdi-account"
+                        type="text"
+                        v-model="newUser.name"
+                    ></v-text-field>
+                    <v-text-field
+                        label="Email"
+                        name="email"
+                        prepend-icon="mdi-email"
+                        type="email"
+                        v-model="newUser.email"
+                    ></v-text-field>
+                    <v-text-field
+                        id="password"
+                        label="Password"
+                        name="password"
+                        prepend-icon="mdi-lock"
+                        type="password"
+                        v-model="newUser.password"
+                    ></v-text-field>
+                    <v-text-field
+                        id="password_confirmation"
+                        label="Password_confirmation"
+                        name="password_confirmation"
+                        prepend-icon="mdi-lock"
+                        type="password"
+                        v-model="newUser.password_confirmation"
+                    ></v-text-field>                
+                    </v-form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" @click="registerUser" >Register</v-btn>
+                </v-card-actions>
+                </v-card>
+            </v-col>
+            </v-row>
+        </v-container>
+        <v-snackbar
+        v-model="snackbar.show"
+        >
+        {{ snackbar.text }}
+
+        <template v-slot:action="{ attrs }">
+            <v-btn
+            color="pink"
+            text
+            v-bind="attrs"
+            @click="snackbar.show = false"
+            >
+            Close
+            </v-btn>
+        </template>
+        </v-snackbar>
+    </div> 
+</template>
+<script>
+
+import axios from 'axios'
+
+export default {
+    name: "Register",
+    data(){
+        return{
+            formValid: false,
+            newUser: {
+                email: '',
+                name:'',
+                password:'',
+                password_confirmation: ''
+            },
+            snackbar:{
+                show: false,
+                text: 'success'
+            }
+        }
+    },
+    methods:{
+        registerUser(){
+            if(this.$refs.registerForm.validate()){
+                console.log(this.newUser);
+                axios
+                    .post('http://127.0.0.1:8001/api/register', this.newUser)
+                    .then((response) => {
+                        if(response.data && response.data.success){
+                            this.snackbar = {
+                                show : true,
+                                text : 'success'
+                            }                        
+                        }
+
+                        this.$router.push({name:'login'});
+                    })
+                    .catch(() => {
+                        this.snackbar = {
+                            show : true,
+                            text : 'Failed!!!'
+                        }
+                    })
+                //console.log({event, $form: this.$refs.registerForm.validate()});
+            }
+
+        }
+    }
+}
+</script>
